abc549f052f904445dbde7422a96f51df09d0348
tataqt <tata_98@ukr.net>
2020-01-22 17:07:00 +0200

    connected image

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import Post from './Post';
+import json from './assets/json';
+import WebpackLogo from './assets/webpack-logo.png'
 import './styles/style.css'
 
-const post = new Post('Webpack post title');
+const post = new Post('Webpack post title', WebpackLogo);
 
 console.log('Post to string', post.toString());
+console.log(json);
