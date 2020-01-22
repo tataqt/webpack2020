8019a95fb9bb883bded80ce5a2d837282bfa89a9
tataqt <tata_98@ukr.net>
2020-01-22 17:30:09 +0200

    connected csv and xml

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Post from './Post';
 import json from './assets/json';
+import xml from './assets/data.xml';
+import csv from './assets/data.csv';
 import WebpackLogo from './assets/webpack-logo.png'
 import './styles/style.css'
 
 const post = new Post('Webpack post title', WebpackLogo);
 
-console.log('Post to string', post.toString());
-console.log(json);
+// console.log('Post to string', post.toString());
+// console.log('JSON', json);
+// console.log('XML', xml);
+// console.log('CSV', csv);
