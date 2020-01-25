c521ae1b80045f5fd0bb181ed59c0d44e624c2ac
tataqt <tata_98@ukr.net>
2020-01-25 15:39:20 +0200

    сделали минификацию для production версии

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,30 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+
+const isDev = process.env.NODE_ENV === 'development';
+
+const optimization = () => {
+    const config = {
+        splitChunks: { // Разбить chunk(подключения)  
+            chunks: 'all'
+        }
+    }
+
+    if (!isDev) { // Минифицировать css
+        config.minimizer = [
+            new OptimizeCssAssetsPlugin(),
+            new TerserPlugin()
+        ]
+    }
+    return config
+}
+
 
 module.exports = {
     context: path.resolve(__dirname, 'src'), // Точка входа
@@ -20,25 +44,41 @@ module.exports = {
             '@': path.resolve(__dirname, 'src')
         }
     },
-    optimization: {
-        splitChunks: { // Разбить chunk(подключения)  
-            chunks: 'all'
-        }
-    },
+    optimization: optimization(),
     devServer: { // Запуск сервера который обновляется при изменение файлов
-        port: 4200
+        port: 4200,
+        hot: isDev
     },
     plugins: [ // Плагины
         new HtmlWebpackPlugin({ //Html плагин для подключения своего шаблона
-            template: './index.html'
+            template: './index.html',
+            minify: {
+                collapseWhitespace: !isDev
+            }
         }),
-        new CleanWebpackPlugin() // Очиста папки в которую билдится проект
+        new CleanWebpackPlugin(), // Очиста папки в которую билдится проект
+        new CopyWebpackPlugin([ // Для переноса любых файлов
+            {
+                from: path.resolve(__dirname, 'src/favicon.ico'),
+                to: path.resolve(__dirname, 'dist')
+            }
+        ]),
+        new MiniCssExtractPlugin({ //Подключаем css в отдельный файл
+            filename: '[name]-[contenthash].css'
+        })
     ],
     module: {
         rules: [ // Правила для подключение различных типов файлов
             {
                 test: /\.css$/, // Подключаем css
-                use: ['style-loader', 'css-loader']
+                use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        hmr: isDev,
+                        reloadAll: true
+                    },
+                }, 'css-loader'
+                ]
             },
             {
                 test: /\.(png|jpg|svg|gif)$/, // Подключаем картинки
